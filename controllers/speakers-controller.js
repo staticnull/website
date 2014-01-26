23b6d8e6200f7ee42a3dc33ed7411ea9e969bfb8
Bobby Warner <bobbywarner@gmail.com>
2014-01-26 15:08:46 -0600

    Removed debug stuff

--- controllers/speakers-controller.js
+++ controllers/speakers-controller.js
@@ -7,23 +7,6 @@ module.exports = {
   },
 
   create: function(req, res) {
-    var fullName = req.body.fullName;
-    var email = req.body.email;
-    var twitter = req.body.twitter;
-    var github = req.body.github;
-    var talkTitle = req.body.talkTitle;
-    var talkAbstract = req.body.talkAbstract;
-    var track = req.body.track;
-    var other = req.body.other;
-    console.log('Name: ' + fullName);
-    console.log('Email: ' + email);
-    console.log('Twitter: ' + twitter);
-    console.log('GitHub: ' + github);
-    console.log('Talk Title: ' + talkTitle);
-    console.log('Talk Abstract: ' + talkAbstract);
-    console.log('Track: ' + track);
-    console.log('Other: ' + other);
-
     (new Talk(req.body)).save(function(err){
       if (err) {
         console.log(err);
