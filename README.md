681103a1fdb11e1b6883f623ce75778b35f60f3d
Kent C. Dodds <kent@doddsfamily.us>
2014-06-23 13:31:04 -0600

    Improving the maintainability of the speakers page and adding social and talk info capabilities

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Source code for midwestjs.com
 ```
 node app
 ```
+
+To add a talk:
+
+1. Add line in `data/talks.js` for the speaker and talk
+1. Add the abstract markdown in `data/talk-abstracts/{name-of-talk}.md`
+1. Add the speaker's picture in `public/images/speakers/{name-of-speaker}.jpg`
+
+Note, the name of the talk and speaker should the same as the talk/speaker with the following changes:
+
+- Lowercase
+- Separated by dashes
+- Remove non-file-safe characters and dots (.)
--- controllers/speakers-controller.js
+++ controllers/speakers-controller.js
@@ -1,9 +1,25 @@
 var Talk = require(APP_ROOT.join('models/talk'));
+var talks = require(APP_ROOT.join('data/talks'));
+
+var groupedTalks = {
+  keynotes: splitIntoGroups(talks.keynotes, 2),
+  other: splitIntoGroups(talks.other, 3)
+};
+
+function splitIntoGroups(arry, size) {
+  var totalGroups = Math.ceil(arry.length / size);
+  var group = [];
+  for (var i = 0; i < totalGroups; i++) {
+    var talkGroup = arry.slice(i * size, (i + 1) * size);
+    group.push(talkGroup);
+  }
+  return group;
+}
 
 module.exports = {
 
   index: function(req, res){
-    res.render('speakers', { title: 'Midwest JS | Speakers', speakersActive: 'active' });
+    res.render('speakers', { title: 'Midwest JS | Speakers', speakersActive: 'active', talks: groupedTalks });
   },
 
   create: function(req, res) {
