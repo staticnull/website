4743acf61b5b167bb827ea4a520ef30b04077329
Bobby Warner <bobbywarner@gmail.com>
2016-01-09 17:26:18 -0600

    Open call for papers

--- config/routes.js
+++ config/routes.js
@@ -9,6 +9,8 @@ fs.readdirSync(APP_ROOT.join('controllers')).forEach(function(filename){
 module.exports = function(app) {
   app.get('/', controllers['main'].index);
   app.get('/speakers', controllers['speakers'].index);
+  app.post('/speakers', controllers['speakers'].create);
+  app.get('/speakers/confirmation', controllers['speakers'].confirmation);
   app.get('/speakers/review', controllers['speakers'].review);
 //  app.get('/speakers/delete', controllers['speakers'].delete);
   app.get('/sponsors', controllers['main'].sponsors);
