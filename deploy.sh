6ba1c36228dbe45c23d393f456514605787bb972
Bobby Warner <bobbywarner@gmail.com>
2016-01-09 17:09:39 -0600

    Add pm2 start/stop to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 ssh -t midwestjs bash -c "'
+sudo systemctl stop nginx
+pm2 stop app
 cd /home/centos/website
 git pull
 npm install
+pm2 start app
+sudo systemctl start nginx
 '"
 
 echo "Midwest JS Deploy Finished"
