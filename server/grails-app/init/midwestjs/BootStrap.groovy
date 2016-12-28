b97743ff9756269d0cd721aac6af73d9b5a4fa02
Mike Truso <mike@agileorbit.com>
2016-12-28 11:41:15 -0600

    removing bootstrap objects

--- server/grails-app/init/midwestjs/BootStrap.groovy
+++ server/grails-app/init/midwestjs/BootStrap.groovy
@@ -1,34 +1,8 @@
 package midwestjs
 
-import com.midwestjs.api.ConferenceFeedback
-import com.midwestjs.api.Talk
-import com.midwestjs.api.Speaker
-import com.midwestjs.api.Feedback
-import com.midwestjs.api.Role
-import com.midwestjs.api.User
-import com.midwestjs.api.UserRole
-
 class BootStrap {
 
     def init = { servletContext ->
-//        Role role = new Role('ROLE_ADMIN').save(failOnError: true)
-//        Role rolePublic = new Role('ROLE_PUBLIC').save(failOnError: true)
-//        User user = new User(username: 'mike@agileorbit.com', password: 'supersecret').save(failOnError: true)
-//        UserRole userRole = new UserRole(user, role).save(failOnError: true)
-//        UserRole userRole2 = new UserRole(user, rolePublic).save(failOnError: true)
-//
-//        Speaker speaker1 = new Speaker(fullName: 'Mike Truso', email: 'mike@agileorbit.com').save(failOnError: true)
-//        Speaker speaker2 = new Speaker(fullName: 'Cory Tranby', email: 'cory@agileorbit.com').save(failOnError: true)
-//        Talk talk1 = new Talk(speaker: speaker1, title: 'Do more with jQuery', approved: true, conferenceYear: 2017).save(failOnError: true)
-//        Talk talk2 = new Talk(speaker: speaker2, title: 'Advanced jQuery', talkAbstract: "\$('#mydiv').parent().parent().parent().html()", approved: false, conferenceYear: 2017).save(failOnError: true)
-//
-//        Feedback feedback = new Feedback(talk: talk1, rating: 4.5).save(failOnError: true)
-//
-//        ConferenceFeedback conferenceFeedback1 = new ConferenceFeedback(overall: 4.5, venue: 4, happyHour: 5, expand: 3, favoriteTalk: talk1).save(failOnError: true)
-//        ConferenceFeedback conferenceFeedback2 = new ConferenceFeedback(overall: 3.25, venue: 3.75, expand: 4.5, favoriteTalk: talk2).save(failOnError: true)
-//        ConferenceFeedback conferenceFeedback3 = new ConferenceFeedback(overall: 4.25, venue: 4.75, expand: 2, favoriteTalk: talk2).save(failOnError: true)
-//        ConferenceFeedback conferenceFeedback4 = new ConferenceFeedback(overall: 4.25, venue: 4.75, expand: 2, favoriteTalk: talk2).save(failOnError: true)
-
     }
     def destroy = {
     }
