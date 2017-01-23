ed9d91c6c2017cd23081fe5d08e7332b67e6cb59
Mike Truso <mike@agileorbit.com>
2017-01-23 15:09:15 -0600

    router changes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import {Routes} from '@angular/router';
 import {MainComponent} from "./main/main.component";
+import {TicketComponent} from "./tickets/tickets.component";
+import {SpeakersComponent} from "./speakers/speakers.component";
+import {ScheduleComponent} from "./schedule/schedule.component";
+import {SponsorsComponent} from "./sponsors/sponsors.component";
+import {CfpComponent} from "./cfp/cfp.component";
 
 export const rootRouterConfig: Routes = [
     {path: '', redirectTo: 'main', pathMatch: 'full'},
     {path: 'main', component: MainComponent},
+    {path: 'cfp', component: CfpComponent},
+    {path: 'register', component: SpeakersComponent},
+    {path: 'schedule', component: ScheduleComponent},
+    {path: 'speakers', component: SpeakersComponent},
+    {path: 'supporters', component: SponsorsComponent},
+    {path: 'tickets', component: TicketComponent},
 ];
 
