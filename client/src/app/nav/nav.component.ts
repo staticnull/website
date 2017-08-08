426e00172c1d275ba3d62e478c1c57fa518dd9b3
Cory Tranby <cory@agileorbit.com>
2017-08-08 11:57:05 -0500

    multiple changes

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -11,11 +11,14 @@ export class NavComponent implements OnInit {
 
   controllers: Array<any>;
 
-  constructor(private navService: NavService, private router: Router) { }
+  constructor(private navService: NavService, private router: Router) {
+    console.log("in the nav")
+  }
 
   ngOnInit(): void {
     this.navService.getNavData().subscribe(applicationData => {
       this.controllers = applicationData.controllers.sort((a: any, b: any) => {
+        
         if (a.name < b.name) {
           return -1;
         } else if (a.name > b.name) {
--- client/src/app/schedule/schedule.component.ts
+++ client/src/app/schedule/schedule.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy} from '@angular/core';
 import {NavService} from '../nav/nav.service';
-import {Route, Router} from '@angular/router';
+import {Route, Router, ActivatedRoute, Params} from '@angular/router';
 import {ApiService} from "../app.service";
 import {Response} from "@angular/http";
 
@@ -8,8 +8,11 @@ import {Response} from "@angular/http";
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.css'],
-  providers: [ApiService]
+  providers: [ApiService],
+  changeDetection: ChangeDetectionStrategy.Default
 })
+
+
 export class ScheduleComponent implements OnInit {
 
   controllers: Array<any>;
@@ -18,11 +21,11 @@ export class ScheduleComponent implements OnInit {
   speaker2: Object = [];
   baseUrl: Object;
 
-  constructor(private navService: NavService, private router: Router, private apiService: ApiService) {
+  constructor(private navService: NavService, private router: Router, private apiService: ApiService, private activatedRoute: ActivatedRoute, private changeDetectorRef: ChangeDetectorRef) {
     this.baseUrl = apiService.baseUrl;
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.navService.getNavData().subscribe(applicationData => {
       this.controllers = applicationData.controllers.sort((a: any, b: any) => {
         if (a.name < b.name) {
@@ -37,32 +40,32 @@ export class ScheduleComponent implements OnInit {
   }
 
   openDetail(id) {
-    console.log(id);
     this.getTalk(id);
   }
+  
 
   getTalk(id){
     this.apiService.get(`/api/talk/findById/`+id)
-        .subscribe((res: Response) => {
-          this.talk = res.json()[0];
-          this.speaker = this.talk['speaker'];
-          console.log(this.speaker['id']);
-          if(this.speaker['id'] == 79) {
-            this.getSecondSpeaker(84)
-          } else if (this.speaker['id'] == 84) {
-            this.getSecondSpeaker(79)
-          } else {
-            this.speaker2 = [];
-          }
-        });
+      .subscribe((res: Response) => {
+        this.talk = res.json()[0];
+        this.speaker = this.talk['speaker'];
+        if(this.speaker['id'] == 79) {
+          this.getSecondSpeaker(84)
+        } else if (this.speaker['id'] == 84) {
+          this.getSecondSpeaker(79)
+        } else {
+          this.speaker2 = [];
+        }
+      });
   }
 
   getSecondSpeaker(id){
     this.apiService.get(`/api/speaker/findById/`+id)
-        .subscribe((res: Response) => {
-          this.speaker2 = res.json()[0];
-        });
+      .subscribe((res: Response) => {
+        this.speaker2 = res.json()[0];
+      });
   }
+  
 
   hasRoute(controllerName: string): boolean {
     return this.router.config.some((route: Route) => {
